1c824f43001e8b4a41e3cb96cbd1f16f3dbd1f50
HigorPereira <higor.pereira@mobrj.com.br>
2022-04-05 13:14:41 -0300

    Tela FilmDetail - Criando o backend para retornar as listas com os filmes dublados e legendados - Grupo de Sessões

--- AppShopping/AppShopping/ViewModels/FilmDetailViewModel.cs
+++ AppShopping/AppShopping/ViewModels/FilmDetailViewModel.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AppShopping.Models;
+using AppShopping.Services;
+using System.Linq;
 
 namespace AppShopping.ViewModels
 {
     public class FilmDetailViewModel
     {
+        public Film Film { get; set; }
+
+        public FilmDetailViewModel()
+        {
+            Film = new CinemaService().GetFilms().First();
+        }
     }
 }
