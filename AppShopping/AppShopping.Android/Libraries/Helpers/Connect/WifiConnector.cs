e27669f3627ecbd26efb48f9e8948d989dc99322
HigorPereira <higor.pereira@mobrj.com.br>
2022-05-04 10:57:57 -0300

    Tela de Wifi -  Configuração da ViewModel, dependências e config em cada plataforma

---
+++ AppShopping/AppShopping.Android/Libraries/Helpers/Connect/WifiConnector.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AppShopping.Helpers.Connect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AppShopping.Droid.Libraries.Helpers.Connect.WifiConnector))]
+namespace AppShopping.Droid.Libraries.Helpers.Connect
+{
+    public class WifiConnector : IWifiConnector
+    {
+        public void ConnectToWifi(string ssid, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
