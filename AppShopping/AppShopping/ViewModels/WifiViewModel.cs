46f0ba8e54af5a18d00923695cfd5fa025228b77
HigorPereira <higor.pereira@mobrj.com.br>
2022-05-04 10:36:14 -0300

    Tela de Wifi - Criação da ViewModel e configuração da View

---
+++ AppShopping/AppShopping/ViewModels/WifiViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AppShopping.Helpers.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppShopping.ViewModels
+{
+    public class WifiViewModel : BaseViewModel
+    {
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public ICommand ConnectToWifiCommand { get; set; }
+
+        public WifiViewModel()
+        {
+            ConnectToWifiCommand = new Command(ConnecToWifi);
+        }
+
+        private void ConnecToWifi(object obj)
+        {
+            
+        }
+    }
+}
