cb6464b2813fa0c2170d0c78b93546f97cfd59dd
HigorPereira <higor.pereira@mobrj.com.br>
2022-05-04 10:13:48 -0300

    Listar Novidades - News - Model, View e ViewModel

---
+++ AppShopping/AppShopping/ViewModels/NewsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AppShopping.Helpers.MVVM;
+using AppShopping.Models;
+using AppShopping.Services;
+using System.Collections.Generic;
+
+namespace AppShopping.ViewModels
+{
+    public class NewsViewModel : BaseViewModel
+    {
+        public NewsService _newsService { get; set; }
+        public List<News> News { get; set; }
+        public NewsViewModel()
+        {
+            _newsService = new NewsService();
+
+            News = _newsService.GetNews();
+        }
+    }
+}
